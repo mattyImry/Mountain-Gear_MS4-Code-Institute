2ab91fa310cb9fdcb21cf9bfe35dbd77c3bd0b8d
mattyImry <matteo.imeri@gmail.com>
2021-06-15 09:31:05 +0000

    Add folders and template to cart app

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def view_cart(request):
+    """
+    View to render cart page
+    """
+
+    return render(request, 'cart/cart.html')
