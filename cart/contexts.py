ee4d2a10b0e517e4396772d1430c2317225a0100
mattyImry <matteo.imeri@gmail.com>
2021-06-19 14:18:41 +0000

    updated context.py and cart.view.py to add number functionality

--- cart/contexts.py
+++ cart/contexts.py
@@ -13,17 +13,43 @@ def cart_contents(request):
     total = 0
     product_count = 0
     cart = request.session.get('cart', {})
+    print(cart)
 
-    for item_id, quantity in cart.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        cart_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
+    for item_id, item_data in cart.items():
+
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
             })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            if 'items_by_size' in item_data:
+                for size, quantity in item_data['items_by_size'].items():
+                    total += quantity * product.price
+                    product_count += quantity
+                    cart_items.append({
+                        'item_id': item_id,
+                        'quantity': quantity,
+                        'product': product,
+                        'size': size,
 
+                    })
+            else:
+                product = get_object_or_404(Product, pk=item_id)
+                for number, quantity in item_data['items_by_number'].items():
+                    total += quantity * product.price
+                    product_count += quantity
+                    cart_items.append({
+                        'item_id': item_id,
+                        'quantity': quantity,
+                        'product': product,
+                        'number': number,
+                    })
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
--- cart/views.py
+++ cart/views.py
@@ -17,12 +17,36 @@ def add_to_cart(request, item_id):
     """
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    number = None
+    if 'product_size' or 'product_number' in request.POST:
+        size = request.POST.get('product_size', None)
+        number = request.POST.get('product_number', None)
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    if size:
+        if item_id in list(cart.keys()):
+            if size in cart[item_id]['items_by_size'].keys():
+                cart[item_id]['items_by_size'][size] += quantity
+            else:
+                cart[item_id]['items_by_size'][size] = quantity
+        else:
+            cart[item_id] = {'items_by_size': {size: quantity}}
+
+    elif number:
+        if item_id in list(cart.keys()):
+            if number in cart[item_id]['items_by_number'].keys():
+                cart[item_id]['items_by_number'][number] += quantity
+            else:
+                cart[item_id]['items_by_number'][number] = quantity
+        else:
+            cart[item_id] = {'items_by_number': {number: quantity}}
+
     else:
-        cart[item_id] = quantity
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+        else:
+            cart[item_id] = quantity
 
     request.session['cart'] = cart
     return redirect(redirect_url)
