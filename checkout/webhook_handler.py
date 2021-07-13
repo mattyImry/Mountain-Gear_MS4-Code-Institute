dfde7284a4a7917b5736037440f1df42e6c8f655
mattyImry <matteo.imeri@gmail.com>
2021-07-13 09:55:57 +0000

    Recommit: Add webhook handler Stripe

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+
+
+class StripeWH_Handler:
+    """
+    Handle the webhooks for Stripe
+    Code taken from Boutique Ado
+    """
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """
+        Handle generic webhook event
+        """
+
+        return HttpResponse(
+            content=f'Webhook Receved: {event["type"]}',
+            status=200)
+        )
