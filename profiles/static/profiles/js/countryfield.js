29791c11e830e0dd2145cdd649da0edb84db6f8b
mattyImry <matteo.imeri@gmail.com>
2021-07-20 13:54:44 +0000

    Updated profile.view.py and templates

---
+++ profiles/static/profiles/js/countryfield.js
@@ -0,0 +1,14 @@
+/* Code from Boutique ado */
+
+let countrySelected = $('#id_default_country').val();
+if(!countrySelected) {
+    $('#id_default_country').css('color', '#92a396');
+};
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', '#92a396');
+    } else {
+        $(this).css('color', '#495057');
+    }
+});
