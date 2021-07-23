7205cb4b3a0f5fd7aaf7e351d301e7a9c1dec650
mattyImry <matteo.imeri@gmail.com>
2021-07-23 08:05:28 +0000

    fixed missing user field in the order admin panel

--- checkout/admin.py
+++ checkout/admin.py
@@ -20,12 +20,12 @@ class OrderAdmin(admin.ModelAdmin):
     """
     inlines = (OrderLineItemAdminInline,)
 
-    readonly_fields = ('order_number', 'user_profile', 'date',
+    readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
                        'sum_total', 'original_cart',
                        'stripe_pid')
 
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number','user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -111,8 +111,8 @@ class StripeWH_Handler:
         if order_exists:
             self._send_confirmation_email(order)
             return HttpResponse(
-                content=f'Webhook received: {event["type"]} | SUCCESS:'
-                        f'Verified order already in database',
+                content=(f'Webhook received: {event["type"]} | SUCCESS:'
+                         f'Verified order already in database'),
                 status=200)
         else:
             order = None
