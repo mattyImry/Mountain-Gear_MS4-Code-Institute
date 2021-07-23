23d2c293826e08e0acf5491691a4eada57749050
mattyImry <matteo.imeri@gmail.com>
2021-07-23 11:32:29 +0000

    Completed add product functionality

--- products/views.py
+++ products/views.py
@@ -85,6 +85,17 @@ def add_product(request):
     View to add product to site
     Code taken from Boutique Ado CI tutorial
     """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
     form = ProductForm()
     template = 'products/add_product.html'
     context = {
