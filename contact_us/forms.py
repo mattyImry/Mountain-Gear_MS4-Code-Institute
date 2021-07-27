3c03b1a3fbb6f8369e6069ac976a733ae21e51c4
mattyImry <matteo.imeri@gmail.com>
2021-07-27 11:47:48 +0000

    Completed contact us app

--- contact_us/forms.py
+++ contact_us/forms.py
@@ -2,14 +2,11 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(
-        min_length=3, max_length=100, label='',
-        widget=forms.TextInput(attrs={'placeholder': 'Name'}))
+    name = forms.CharField(min_length=3,
+                           max_length=100, label='Name',
+                           widget=forms.TextInput())
 
-    email = forms.EmailField(
-        widget=forms.EmailInput(
-            attrs={'placeholder': 'Your email address'}), label='')
+    email = email = forms.EmailField(widget=forms.EmailInput(),
+                                     label='Your Email Address')
 
-    message = forms.CharField(
-        widget=forms.Textarea(
-            attrs={'placeholder': 'Your message'}), label='')
+    message = forms.CharField(widget=forms.Textarea(), label='Message')
