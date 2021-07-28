5aecfaa7226f47589618adf4d0c52ab54a0c5322
mattyImry <matteo.imeri@gmail.com>
2021-07-28 11:27:41 +0000

    Add subject field to contact form

--- contact_us/forms.py
+++ contact_us/forms.py
@@ -8,5 +8,7 @@ class ContactForm(forms.Form):
 
     email = email = forms.EmailField(widget=forms.EmailInput(),
                                      label='Your Email Address')
-
+    subject = forms.CharField(min_length=3,
+                              max_length=40, label='Subject',
+                              widget=forms.TextInput())
     message = forms.CharField(widget=forms.Textarea(), label='Message')
