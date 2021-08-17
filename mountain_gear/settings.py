48a21516b9ad2fbab71b79e6f3fffdfc18daeaa7
mattyImry <matteo.imeri@gmail.com>
2021-08-17 13:03:15 +0000

    Remove secret key and debug

--- mountain_gear/settings.py
+++ mountain_gear/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)ve6ft9lk3gmsp%l!*40x-*=y+!fmv0#wnxz9yg%((o0aps*@-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['mountain-gear-ms4.herokuapp.com', 'localhost']
 
