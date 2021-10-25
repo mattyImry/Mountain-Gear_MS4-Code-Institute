3bb5463b0e77b46bdb2d0a376878bae2117194b8
mattyImry <matteo.imeri@gmail.com>
2021-10-25 14:58:27 +0000

    Start test ReviewForm

---
+++ reviews/test_form.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from .forms import ReviewForm
+
+
+class TestReviewForm(TestCase):
+
+    def test_review_is_required(self):
+        form = ReviewForm({'review': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('review', form.errors.keys())
+        self.assertEqual(form.errors['review'][0], 'This field is required.')
