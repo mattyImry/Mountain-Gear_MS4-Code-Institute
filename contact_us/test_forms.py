aa1adf7d9687e3f47f5cd208affda7db3b05436a
mattyImry <matteo.imeri@gmail.com>
2021-10-25 08:55:48 +0000

    Tested completed for contact us form

--- contact_us/test_forms.py
+++ contact_us/test_forms.py
@@ -15,3 +15,15 @@ class TestItemForm(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn('email', form.errors.keys())
         self.assertEqual(form.errors['email'][0], 'This field is required.')
+
+    def test_subject_is_required(self):
+        form = ContactForm({'subject': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('subject', form.errors.keys())
+        self.assertEqual(form.errors['subject'][0], 'This field is required.')
+
+    def test_message_required(self):
+        form = ContactForm({'message': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('message', form.errors.keys())
+        self.assertEqual(form.errors['subject'][0], 'This field is required.')
