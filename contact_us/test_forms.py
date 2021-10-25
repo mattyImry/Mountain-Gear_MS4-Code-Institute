ddd98f581888d56c1712206e2c8769a177a97e7f
mattyImry <matteo.imeri@gmail.com>
2021-10-25 08:35:46 +0000

    Started testing contact_us form

---
+++ contact_us/test_forms.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from .forms import ContactForm
+
+
+class TestItemForm(TestCase):
+
+    def test_item_name_is_required(self):
+        form = ContactForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
+
+    def test_email_address_is_required(self):
+        form = ContactForm({'email': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('email', form.errors.keys())
+        self.assertEqual(form.errors['email'][0], 'This field is required.')
