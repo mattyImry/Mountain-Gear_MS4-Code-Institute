d6a29c6ec652d717b806395ec693c067cd839378
mattyImry <matteo.imeri@gmail.com>
2021-10-27 15:57:45 +0000

    Testing wishlist views

--- reviews/test_model.py
+++ reviews/test_model.py
@@ -1,11 +1,9 @@
 from django.test import TestCase
-from .models import Review, UserProfile, Product
-from django.shortcuts import get_object_or_404
+from .models import Review
+
 
 
 class TestModel(TestCase):
 
     def test_review_model(self):
         rev = Review.objects.create(review="sometxt")
-        
-    
